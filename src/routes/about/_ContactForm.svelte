1419cc091183f3ab1dcd54f58080ba21cf1c6ea1
threesam <salvatoredangelo@protonmail.com>
2020-11-12 06:49:12 -0500

    refactored about section

---
+++ src/routes/about/_ContactForm.svelte
@@ -0,0 +1,33 @@
+<style>
+	div {
+		border: 2px solid black;
+		padding: 2rem;
+	}
+</style>
+
+<div>
+
+	<h2>Contact Me!</h2>
+	<form name="contact" method="POST" data-netlify="true" netlify-honeypot=“bot-field”>
+		<input type="hidden" name="form-name" value="contact" />
+		<p>
+			<label>Your Name: <input type="text" name="name" /></label>   
+		</p>
+		<p>
+			<label>Your Email: <input type="email" name="email" /></label>
+		</p>
+		<p>
+			<label>Your Role: <select name="role[]" multiple>
+				<option value="leader">Leader</option>
+				<option value="follower">Follower</option>
+			</select></label>
+		</p>
+		<p>
+			<label>Message: <textarea name="message"></textarea></label>
+		</p>
+		<p>
+			<button type="submit">Send</button>
+		</p>
+	</form>
+
+</div>
---
+++ src/routes/about/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import ContactForm from "./_ContactForm.svelte"
+</script>
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
+
+<ContactForm/>
+  
