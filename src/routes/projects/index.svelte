af35c2b6771b0fd8ed72c0f798da385b7c59ea3e
threesam <salvatoredangelo@protonmail.com>
2020-11-12 07:30:12 -0500

    made projects page/folder and linked it json to CMS

---
+++ src/routes/projects/index.svelte
@@ -0,0 +1,34 @@
+<script context="module">
+	export function preload() {
+		return this.fetch(`projects.json`).then(r => r.json()).then(projects => {
+			return { projects };
+		});
+	}
+</script>
+
+<script>
+	export let projects;
+</script>
+
+<style>
+	ul {
+		margin: 0 0 1em 0;
+		line-height: 1.5;
+	}
+</style>
+
+<svelte:head>
+	<title>Project</title>
+</svelte:head>
+
+<h1>Recent projects</h1>
+
+<ul>
+	{#each projects as project}
+		<!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+		<li><a rel="prefetch" href="projects/{project.slug}">{project.title}</a></li>
+	{/each}
+</ul>
