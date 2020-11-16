9861d3f7eda1868229c8ba54373572e68413693b
threesam <salvatoredangelo@protonmail.com>
2020-11-15 21:57:19 -0500

    made index the blog, and about only nav options

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,34 @@
+<script context="module">
+	export function preload() {
+		return this.fetch(`index.json`).then(r => r.json()).then(posts => {
+			return { posts };
+		});
+	}
+</script>
+
 <script>
-	import successkid from 'images/successkid.jpg';
+	export let posts;
 </script>
 
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
+	ul {
 		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
+		line-height: 1.5;
 	}
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Blog</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Recent posts</h1>
 
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<ul>
+	{#each posts as post}
+		<!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+		<li><a rel="prefetch" href="blog/{post.slug}">{post.title}</a></li>
+	{/each}
+</ul>
