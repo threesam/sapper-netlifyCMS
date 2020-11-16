89099829bbe1120c7936b1edd644e0b72bbde6bb
threesam <salvatoredangelo@protonmail.com>
2020-11-15 21:45:46 -0500

    trimmed nav to home-blog, about

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -51,8 +51,8 @@
 <nav>
 	<ul>
 		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-		<li><a aria-current="{segment === 'projects' ? 'page' : undefined}" href="projects">projects</a></li>
+		<!-- <li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li> -->
+		<!-- <li><a aria-current="{segment === 'projects' ? 'page' : undefined}" href="projects">projects</a></li> -->
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
